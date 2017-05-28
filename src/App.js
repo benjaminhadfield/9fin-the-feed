9c7dc33200a859565873cb6c68826a9fdf706aec
Ben Hadfield <Ben@hadfieldfamily.co.uk>
2017-05-28 14:19:13 +0100

    added not-found gif 😉

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React from 'react'
 import {
   BrowserRouter as Router,
   Route,
-  Link
+  Link,
+  Switch
 } from 'react-router-dom'
 // import logo from './logo.svg'
 import Tabs from './components/tabs'
@@ -10,15 +11,19 @@ import Header from './components/header'
 import Updates from './pages/updates'
 import Reviews from './pages/reviews'
 import Cats from './pages/cats'
+import NotFound from './pages/errors/NotFound'
 
 export default () => (
   <Router>
     <div>
       <Header />
       <Tabs tabs={[<Link to='/'>Updates</Link>, <Link to='/reviews'>Reviews</Link>, <Link to='/cats'>Cats</Link>]}>
-        <Route exact path='/' component={Updates} />
-        <Route path='/reviews' component={Reviews} />
-        <Route path='/cats' component={Cats} />
+        <Switch>
+          <Route exact path='/' component={Updates} />
+          <Route path='/reviews' component={Reviews} />
+          <Route path='/cats' component={Cats} />
+          <Route component={NotFound} />
+        </Switch>
       </Tabs>
     </div>
   </Router>
---
+++ src/pages/errors/NotFound/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Heading1, P } from '../../../components/text'
+import { Link } from 'react-router-dom'
+import NotFoundGif from '../../../assets/not-found.gif'
+
+export default () => (
+  <div>
+    <P><img src={NotFoundGif} alt={'This is not the page you\'re looking for'} height={240} /></P>
+    <Link to='/'>Go back home</Link>
+  </div>
+)
