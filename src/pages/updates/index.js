a47679abedcf4be35172af126ba9a8d6e88bc7cf
Ben Hadfield <Ben@hadfieldfamily.co.uk>
2017-05-29 00:09:07 +0100

    in component websocket connection

--- src/pages/updates/index.js
+++ src/pages/updates/index.js
@@ -2,24 +2,37 @@ import React from 'react'
 import Spinner from '../../components/spinner'
 import { connect } from 'react-redux'
 import { Tweet } from '../../components/list'
+import io from 'socket.io-client'
 
-const Updates = ({loading, travel}) => {
-  return (
-    <div>
-      { loading
-        ? <Spinner name='three-bounce' />
-        : travel.map(t =>
-          <Tweet
-            key={t.id}
-            avatar={t.user.profile_image_url_https}
-            name={t.user.name}
-            profileLink={t.user.url}
-            location={t.user.location}
-            time={t.created_at}
-            content={t.text} />)
-      }
-    </div>
-  )
+const socket = io('http://the-london-feed.herokuapp.com/travel')
+
+class Updates extends React.Component {
+  componentDidMount () {
+    console.log('connecting...')
+    socket.on('connect', () => {
+      console.log('connected to /travel', socket.id)
+    })
+  }
+
+  render () {
+    const {loading, travel} = this.props
+    return (
+      <div>
+        { loading
+          ? <Spinner name='three-bounce' />
+          : travel.map(t =>
+            <Tweet
+              key={t.id}
+              avatar={t.user.profile_image_url_https}
+              name={t.user.name}
+              profileLink={t.user.url}
+              location={t.user.location}
+              time={t.created_at}
+              content={t.text} />)
+        }
+      </div>
+    )
+  }
 }
 
 const mapStateToProps = state => {
